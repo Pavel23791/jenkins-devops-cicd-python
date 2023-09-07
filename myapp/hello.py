81df1a31911addc8e3dd82a5cd8f8f8defc85319
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2023-09-07 19:57:07 +0300

    add  multi

--- myapp/hello.py
+++ myapp/hello.py
@@ -3,5 +3,10 @@ import fire
 def hello(name="World"):
   return "Hello %s!" % name
 
+def multi(number1=0, number2=0):
+  multnum = number1 * number2
+  return f"The mult of {number1} and {number2} is {multnum}"
+
 if __name__ == '__main__':
   fire.Fire(hello)
+  fire.Fire(multi)
